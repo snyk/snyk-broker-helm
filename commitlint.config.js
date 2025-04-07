1f28829963feffb0853f8faa8ea97d8bbfa0280c
Shuaib Munshi <shuaib.munshi@snyk.io>
2025-04-07 16:44:48 -0400

    chore: Missing commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// https://commitlint.js.org/#/reference-rules
+// Conventional commit rules: https://github.com/conventional-changelog/commitlint/tree/4c9928ecc77d04deb40e36cec669853558aab03b/%40commitlint/config-conventional
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
